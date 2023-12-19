3f776ee18da1177cba57d27d1badc277bbfce01d
DeboshedFish <132343567+DeboshedFish@users.noreply.github.com>
2023-12-19 17:27:51 +0100

    Le code utilise la fonction compare qui utilise le hachage perceptif. La prochaine étape est de répartir cette fonction avec 3 threads pour rendre l'exécution plus rapide

--- serveur/main.c
+++ serveur/main.c
@@ -1,3 +1,6 @@
+// server - img-search
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,6 +10,9 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+#include <inttypes.h>
+
+#include "imgdist.h"
 
 #define PORT 5555
 #define MAX_SIZE 20480 // 20KB, limit of image size
@@ -18,38 +24,7 @@ typedef struct {
     struct sockaddr_in client_addr;
 } ThreadArgs;
 
-bool readImage(const char *filename, char **imageData, size_t *imageSize) {
-    FILE *file = fopen(filename, "rb");
-    if (file == NULL) {
-        perror("Error opening file for reading");
-        return false;
-    }
 
-    // Get file size
-    fseek(file, 0, SEEK_END);
-    *imageSize = ftell(file);
-    rewind(file);
-
-    // Allocate memory for image data
-    *imageData = (char *)malloc(*imageSize);
-    if (*imageData == NULL) {
-        perror("Error allocating memory for image data");
-        fclose(file);
-        return false;
-    }
-
-    // Read image data from file
-    size_t bytesRead = fread(*imageData, 1, *imageSize, file);
-    if (bytesRead != *imageSize) {
-        perror("Error reading image data");
-        free(*imageData);
-        fclose(file);
-        return false;
-    }
-
-    fclose(file);
-    return true;
-}
 bool readImage(const char *filename, char **imageData, size_t *imageSize) {
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
@@ -85,69 +60,98 @@ bool readImage(const char *filename, char **imageData, size_t *imageSize) {
 
 	
 void* compare(const char* raw_image_name) {
-    printf("On entre dans la fonction compare\n");
+    printf("IT IS WORKING\n");
 
+	uint64_t raw_hash;  // Declare raw_hash here
+	
     if (raw_image_name != NULL) {
         printf("raw_image_name: %s\n", raw_image_name);
 
-        char *imageData;
+        char* imageData;
         size_t imageSize;
 
+        bool raw_result;
+
         if (readImage(raw_image_name, &imageData, &imageSize)) {
-            uint64_t raw_hash;
-            int raw_result;
-            
-            if (PHashRaw(imageData, imageSize, &raw_hash)) {
-                raw_result = raw_hash;
-                fprintf("raw_result: %i", raw_result);
+            // Assuming PHashRaw is declared somewhere
+            if ((raw_result = PHashRaw(imageData, imageSize, &raw_hash))) {
+                printf("raw_hash: %" PRIu64 "\n", raw_hash);
 
                 // Continue with the rest of the code...
             } else {
                 fprintf(stderr, "Error couldn't hash raw image\n");
+                // Don't forget to free the allocated memory
+                free(imageData);
+                return NULL;
             }
 
             // Don't forget to free the allocated memory
             free(imageData);
         } else {
             fprintf(stderr, "Error reading image data\n");
+            return NULL;
         }
     } else {
         printf("raw_image_name is NULL\n");
+        return NULL;
     }
-	
-	/*
-	 
-    // increment it by three
+
+
+    // Assuming these variables are declared and initialized
+    uint64_t db_result;
+    int final_result = 999;
+    
+    const char* best_name = NULL;
+
+    printf("Before for loop\n");
+
+   // increment it by three when using threads
     for (int dbimg_nb = 1; dbimg_nb < 102; dbimg_nb++) {
+		printf("Entering loop for database number: %d\n", dbimg_nb);
+		// Add a volatile variable or function call
+		
         char nb_to_str[4];
         sprintf(nb_to_str, "%d", dbimg_nb);
 
+        char dbimg_name[8];
         char suffix[] = ".bmp";
 
         // concatenate to obtain nb.bmp
-        char result[7];
         sprintf(dbimg_name, "%s%s", nb_to_str, suffix);
+        
+        
+        char final_name[12];
+        char prefix[] = "img/";
+        
+        sprintf(final_name, "%s%s", prefix, dbimg_name);
+        printf("db image name that is being compared: %s\n", final_name); 
 
-        if (PHash(dbimg_name, &db_hash)){
+        uint64_t db_hash;
+        if (PHash(final_name, &db_hash)) {
             db_result = db_hash;
         } else {
             perror("Error couldn't hash database image");
+            // Handle error and continue or return, depending on your logic
+            continue;
         }
 
-        int distance = DistancePHash(raw_result, db_result);
-        if (final_result < distance) {
+        int distance = DistancePHash(raw_hash, db_result);
+        if (final_result > distance) {
             final_result = distance;
-            best_name = dbimg_name;
+            best_name = final_name;
+            printf("The closest image so far is: %s with a distance of %d\n,", best_name, distance);
         }
     }
-    printf("Most similar image found: %s with a distance of %d.\n", best_name, final_result);
-    printf("IT IS WORKING\n");
     
-    */
-    
-    return NULL;
-   
+    if (best_name != NULL) {
+        printf("Most similar image found: %s with a distance of %d\n", best_name, final_result);
+    } else {
+        printf("No similar image found.\n");
+    }
+    return NULL; 
 }
+
+
 
 
 // Function to handle a connected client
@@ -164,7 +168,7 @@ void *handleClient(void *arg) {
 
     while (1) {
         // Receive image data from the client and save it based on the client socket
-        char filename[255];
+        char filename[40];
         sprintf(filename, "serveur/received_image_%d.bmp", threadArgs->csock);
         FILE *file = fopen(filename, "wb");
 
@@ -187,9 +191,11 @@ void *handleClient(void *arg) {
         fclose(file);
         printf("File received and saved as %s\n", filename);
 
-	// !!! COMPARISON
+		
+		// THE WHILE(1) LOOP HAS CHOSEN TO LOOP AGAIN BEFORE PRINTING THE FOLLOWING
+		// !!! COMPARISON
         printf("BEFORE COMPARISON\n");
-        compare(&filename);
+		compare(filename);
         printf("AFTER COMPARISON");
 		
         // Send a success message to the client
